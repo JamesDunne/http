b9d8456800521a0ae28f5f8a3a29a1219371586b
James Dunne <james.jdunne@gmail.com>
2015-02-26 17:53:39 -0600

    Ugh. Cannot modify bash env vars. Output scripts to do so.

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"sort"
 	"strings"
 )
 
@@ -17,37 +18,117 @@ func Error(format string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, format, a...)
 }
 
-func do_http(http_method string, body_required bool, args []string) {
-	// Validate environment variables first:
+func output_bash_env(env_names []string) {
+	// Output the bash script to modify the environment:
+	for _, key := range env_names {
+		value := os.Getenv(key)
+		// Bash-escape the value as a single-quoted string with escaping rules:
+		bash_escape_value := strings.Replace(value, "\\", "\\\\", -1)
+		bash_escape_value = strings.Replace(bash_escape_value, "'", "\\'", -1)
+		fmt.Printf("export %s=$'%s'\n", key, bash_escape_value)
+	}
+	fmt.Println()
+	fmt.Println("# This output must be `eval`ed in bash in order to have effect on the current environment.")
+	fmt.Println("# Example: $ eval `http-cli ...`")
+}
+
+func get_abs_url() *url.URL {
 	url_s := os.Getenv("HTTPCLI_URL")
 	if url_s == "" {
 		Error("Missing $HTTPCLI_URL env var\n")
 		os.Exit(2)
-		return
+		return nil
 	}
 	base_url, err := url.Parse(url_s)
 	if err != nil {
 		Error("Error parsing $HTTPCLI_URL: %s\n", err)
 		os.Exit(2)
-		return
+		return nil
 	}
 	if !base_url.IsAbs() {
 		Error("$HTTPCLI_URL must be an absolute URL\n")
 		os.Exit(2)
+		return nil
+	}
+	return base_url
+}
+
+func set_abs_url(base_url *url.URL) {
+	err := os.Setenv("HTTPCLI_URL", base_url.String())
+	if err != nil {
+		Error("Error setting $HTTPCLI_URL: %s\n", err)
+		os.Exit(2)
 		return
 	}
+}
 
-	// Get extra headers:
+func get_headers() http.Header {
+	// Get HTTP headers from environment:
 	headers_s := os.Getenv("HTTPCLI_HEADERS")
-	headers := make(map[string]string)
+	headers := make(http.Header)
 	if headers_s != "" {
 		err := json.Unmarshal([]byte(headers_s), &headers)
 		if err != nil {
 			Error("Error parsing JSON from $HTTPCLI_HEADERS: %s\n", err)
 			os.Exit(2)
-			return
+			return headers
 		}
 	}
+	return headers
+}
+
+type keyValues struct {
+	key    string
+	values []string
+}
+
+// A headerSorter implements sort.Interface by sorting a []keyValues
+// by key. It's used as a pointer, so it can fit in a sort.Interface
+// interface value without allocation.
+type headerSorter struct {
+	kvs []keyValues
+}
+
+func (s *headerSorter) Len() int           { return len(s.kvs) }
+func (s *headerSorter) Swap(i, j int)      { s.kvs[i], s.kvs[j] = s.kvs[j], s.kvs[i] }
+func (s *headerSorter) Less(i, j int) bool { return s.kvs[i].key < s.kvs[j].key }
+
+// sortedKeyValues returns h's keys sorted in the returned kvs
+// slice. The headerSorter used to sort is also returned, for possible
+// return to headerSorterCache.
+func sortedKeyValues(h http.Header) (kvs []keyValues) {
+	kvs = make([]keyValues, 0, len(h))
+	for k, vv := range h {
+		kvs = append(kvs, keyValues{k, vv})
+	}
+
+	hs := &headerSorter{
+		kvs: kvs,
+	}
+	sort.Sort(hs)
+
+	return hs.kvs
+}
+
+func set_headers(headers http.Header) {
+	headers_s, err := json.Marshal(headers)
+	if err != nil {
+		Error("Error marshalling JSON to $HTTPCLI_HEADERS: %s\n", err)
+		os.Exit(2)
+		return
+	}
+	err = os.Setenv("HTTPCLI_HEADERS", string(headers_s))
+	if err != nil {
+		Error("Error setting $HTTPCLI_HEADERS: %s\n", err)
+		os.Exit(2)
+		return
+	}
+}
+
+func do_http(http_method string, body_required bool, args []string) {
+	// Get environment:
+	base_url := get_abs_url()
+	headers := get_headers()
 
 	if len(args) < 1 {
 		Error("Missing required relative URL\n")
@@ -78,11 +159,7 @@ func do_http(http_method string, body_required bool, args []string) {
 	req := &http.Request{
 		URL:    api_url,
 		Method: http_method,
-		Header: make(http.Header),
-	}
-
-	for key, value := range headers {
-		req.Header.Set(key, value)
+		Header: headers,
 	}
 
 	// Exclude named headers:
@@ -139,14 +216,17 @@ func main() {
 http-cli <command> [args...]
 
 Commands:
-  url    <absolute_url>
-    Set base URL in environment.
+  url    [absolute_url]
+    Get or set base URL in environment.
 
   header-clear
     Clears all HTTP headers in environment.
 
   header-set <header_name> <header_value>
     Sets a custom HTTP header in environment.
+
+  header-list
+    List current HTTP headers in environment.
 
   GET    <relative-url>
   DELETE <relative-url>
@@ -169,20 +249,67 @@ Environment variables:
 
 	// Determine what to do:
 	cmd := args[0]
+	args = args[1:]
+
 	body_required := true
 	switch strings.ToLower(cmd) {
 	case "get", "delete":
 		body_required = false
 		fallthrough
 	case "post", "put":
-		do_http(strings.ToUpper(cmd), body_required, args[1:])
+		do_http(strings.ToUpper(cmd), body_required, args)
 		break
 
 	case "url":
+		// Must be evaluated on the bash console as "eval `http-cli header-set ...`"
+		if len(args) == 0 {
+			base_url := get_abs_url()
+			fmt.Printf("%s\n", base_url)
+		} else if len(args) == 1 {
+			base_url, err := url.Parse(args[0])
+			if err != nil {
+				Error("Error parsing absolute URL: %s\n", err)
+				os.Exit(1)
+				return
+			}
+			set_abs_url(base_url)
+			output_bash_env([]string{"HTTPCLI_URL"})
+		}
 		break
+
+	case "header-list":
+		// Get HTTP headers from environment:
+		headers := get_headers()
+		for key, values := range headers {
+			fmt.Printf("%s: %s\n", key, strings.Join(values, " "))
+		}
+		break
+
 	case "header-set":
+		// Must be evaluated on the bash console as "eval `http-cli header-set ...`"
+
+		// Get HTTP headers from environment:
+		headers := get_headers()
+		if len(args) == 2 {
+			// Set a new HTTP header:
+			headers.Set(args[0], args[1])
+		} else if len(args) == 1 {
+			delete(headers, args[0])
+		} else {
+
+		}
+		set_headers(headers)
+
+		// Output the bash evaluation statements:
+		output_bash_env([]string{"HTTPCLI_HEADERS"})
 		break
+
 	case "header-clear":
+		// Must be evaluated on the bash console as "eval `http-cli header-clear ...`"
+		set_headers(nil)
+
+		// Output the bash evaluation statements:
+		output_bash_env([]string{"HTTPCLI_HEADERS"})
 		break
 
 	default:
