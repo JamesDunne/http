35886194922cf2e86abbf01a4b4c67e771b38512
James Dunne <james.jdunne@gmail.com>
2015-03-02 16:23:28 -0600

    Parameterized example script.

--- example.sh
+++ example.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Please supply a 40-digit GitHub access token for this example."
+  exit
+fi
+
+echo "Session ID for this script:"
+http session
+echo
+
 http set Accepts application/vnd.travis-ci.2+json
 http set User-Agent http-cli/0.1
 http url https://api.travis-ci.com
-echo -n '{"github_token":"A-40-CHARACTER-HEXADECIMAL-GITHUB-TOKEN-"}' | http post auth/github
+echo -n "{\"github_token\":\"$1\"}" | http post auth/github 2>/dev/null
+echo
+
+if (( $? == 0 )); then
+	echo Success
+else
+	echo Failed
+fi
